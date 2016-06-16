ce77049c94f1e8d3c973f272aaf54ff8c7b119ce
Jess Dunham <jessdunham@rg43053.rgu.ac.uk>
2016-06-16 15:17:30 +0100

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Jess Dunham on 16/06/2016.
+//  Copyright © 2016 Jess Dunham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
